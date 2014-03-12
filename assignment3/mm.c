c3a72610680250f4e270d48f2434207fb0f59d35
Jordan Anderson <and02930@umn.edu>
2014-03-11 20:00:57 -0500

    alloc started

--- assignment3/mm.c
+++ assignment3/mm.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "mm.h"
 
+static struct spot head;
+
 int mm_init(unsigned long size) {
     /* create a memory pool and initialize data structure */
 
@@ -9,7 +11,7 @@ int mm_init(unsigned long size) {
 		return -1;
 	struct spot temp = {NULL, NULL, NULL, NULL, NULL};
 	temp.ptr = start;
-	temp.free = true;
+	temp.free = 1;
 	temp.size = size;
 	head = temp;
 	return 0;
@@ -19,6 +21,25 @@ int mm_init(unsigned long size) {
 
 char *mm_alloc(unsigned long nbytes) {
     /* allocate nbytes memory space from the pool using first fit algorithm, and return it to the requester */
+	struct spot current = head;
+	while(!(current)) {
+		if(current.free) {
+			if(current.size >= nbytes) {
+				unsigned long diff = nbytes - current.size;
+				if(diff) {
+					struct spot freespace = {NULL, NULL, NULL, NULL, NULL}
+					freespace.ptr = &current.ptr + nbytes;
+					freespace.free = 1;
+					freespace.size = diff;
+					freespace.next = current.next;
+					freespace.prev = current;
+					current.next = freespace;
+				}
+				current.free = 0;
+				return  current.ptr;
+			}
+		}
+	}
 }
 
 
