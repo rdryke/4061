a01173cbc9e2d4ca5320545dedd76858f9978dda
Rob Dryke <dryke014@umn.edu>
2014-03-11 20:05:57 -0500

    free added

--- assignment3/mm.c
+++ assignment3/mm.c
@@ -6,7 +6,9 @@ int mm_init(unsigned long size) {
 
 	char* start = (char*)malloc(sizeof(char)*size);
 	if(!(start))
+	{
 		return -1;
+	}
 	struct spot temp = {NULL, NULL, NULL, NULL, NULL};
 	temp.ptr = start;
 	temp.free = true;
@@ -23,11 +25,40 @@ char *mm_alloc(unsigned long nbytes) {
 
 
 int mm_free(char *ptr) {
+	struct spot current = head;
+	while (!(current))
+	{
+		if (current.ptr = ptr)
+		{
+			if (current.prev != NULL && current.prev.free)
+			{
+				current.prev.size += current.size;
+				current.prev.next = current.next;
+				current = current.prev;
+			}
+			else
+			{
+				current.free = 1;
+			}
+			if (current.next != NULL && current.next.free == 1)
+			{
+				current.size += current.next.size;
+				current.next = current.next.next;
+			}
+			return 0;
+		}
+	current = current.next;
+	}
+	printf("Free error: not the right pointer!\n");
+	return -1;
 	/* Check if ptr is valid or not. Only free the valid pointer. Defragmentation. */
 }
 
 void mm_end(unsigned long *alloc_num, unsigned long *free_num) {
 	/* Count total allocated blocks. Count total free blocks. Clean up data structure. Free memory pool */
+	int nallocated = 0;
+	int nfree = 0;
+	while (!(current))
 }
 
 
